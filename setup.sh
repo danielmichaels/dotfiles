8aadb2ebdcedd09c44091ddeb03100e3501f4eb2
Daniel Michaels <dan@danielms.site>
2023-11-18 21:34:52 +1100

    chezmoi updates

--- setup.sh
+++ setup.sh
@@ -27,4 +27,5 @@ if ! command -v zsh &>/dev/null; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	chmod -R 755 ~/.oh-my-zsh/
 fi
