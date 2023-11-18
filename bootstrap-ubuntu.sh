ddc3c871f96ecdc59615fc831688585a498e7a07
Daniel Michaels <dan@danielms.site>
2023-11-18 13:54:02 +1100

    chezmoi updates

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -49,7 +49,7 @@ k8s-tooling() {
 	if ! command -v arkade &>/dev/null; then
 		curl -sLS https://get.arkade.dev | sudo sh
 	fi
-	for app in "${apps[@]}"; do
+	for app in "${arkade_tools[@]}"; do
 		echo "installing $app"
 		arkade get "${app}"
 	done
