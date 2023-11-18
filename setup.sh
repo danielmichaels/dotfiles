b55f9a0c02b0225742f6ee892c873a5a4df87b02
Daniel Michaels <dan@danielms.site>
2023-11-18 21:27:34 +1100

    chezmoi updates

--- setup.sh
+++ setup.sh
@@ -25,5 +25,6 @@ $HOME/.local/share/rtx/installs/chezmoi/latest/bin/chezmoi "${chezmoi_args[@]}"
 if ! command -v zsh &>/dev/null; then
 	sudo apt-get install zsh -y
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-	chsh -s /usr/bin/zsh
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
