378d087d027ff4c5b0bc8ba79938a6684b67b36a
Daniel Michaels <dan@danielms.site>
2023-11-18 12:27:18 +1100

    wip

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dotfiles
+
+> managed by [chezmoi](https://www.chezmoi.io)
+
+## New machine
+
+To get started run the following:
+
+
--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,6 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euox pipefail
 
-# install pre-req's
-sudo apt install zsh vim curl -y
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if ! command -v rtx &>/dev/null; then
+	curl https://rtx.pub/install.sh | sh
+fi
+
+rtx install chezmoi && rtx use chezmoi
+
+chezmoi_args=(
+	init
+	danielmichaels
+	--apply
+	--depth 1
+	--force
+	--keep-going
+)
+
+if [[ ${CHEZMOI_PURGE:-0} == 1 ]]; then
+	chezmoi_args+=(--purge)
+fi
+
+chezmoi "${chezmoi_args[@]}"
+
+if ! command -v zsh &>/dev/null; then
+	sudo apt install zsh
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+	chsh -s zsh
+fi
