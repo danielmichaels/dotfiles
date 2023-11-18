6cd7db95ef158916171f4039289e452c1d09d2ca
Daniel Michaels <dan@danielms.site>
2023-11-18 22:10:48 +1100

    chezmoi updates

--- setup/goinstall.sh
+++ setup/goinstall.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-go install github.com/danielmichaels/gpt/cmd/gpt@latest
-go install github.com/danielmichaels/ds/cmd/ds@latest
-go install github.com/danielmichaels/zet-cmd/cmd/zet@latest
-go install github.com/aandrew-me/tgpt@latest
-go install github.com/charmbracelet/mods@latest
-go install github.com/charmbracelet/glow@latest
-go install github.com/cosmtrek/air@latest
-go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
-go install github.com/azimjohn/jprq/cli@latest
-go install github.com/rs/curlie@latest
+apps=(
+	danielmichaels/gpt/cmd/gpt@latest
+	danielmichaels/ds/cmd/ds@latest
+	danielmichaels/zet-cmd/cmd/zet@latest
+	aandrew-me/tgpt@latest
+	charmbracelet/mods@latest
+	charmbracelet/glow@latest
+	cosmtrek/air@latest
+	sqlc-dev/sqlc/cmd/sqlc@latest
+	azimjohn/jprq/cli@latest
+	rs/curlie@latest
+)
+
+for i in "${apps[@]}"; do
+	printf "Installing: %s\n" "${i}"
+	go install "github.com/${i}"
+done
