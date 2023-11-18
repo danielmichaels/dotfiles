dbf7b12e23fa40c5c390b0e638501a49830649ee
Daniel Michaels <dan@danielms.site>
2023-11-18 12:10:34 +1100

    add bootstrap and startship.toml

---
+++ bootstrap-ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# install pre-req's
+sudo apt install zsh vim curl -y
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
